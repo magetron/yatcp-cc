b32bc7e25ba02922ca555e0c34da6d4f73280fbc
Patrick Wu <magetron1@gmail.com>
2020-06-18 01:05:31 +0100

    update comm with pkt receiver

--- src/comm.cc
+++ src/comm.cc
@@ -32,5 +32,54 @@ void init_udp_socket (node_t *node) {
 	node -> udp_sock_fd = udp_sock_fd;
 }
 
+// TODO
+static void _pkt_receive (node_t* node, char *pkt_with_aux_data, unsigned short bytes_recvd) {
+	return;
+}
+
+static void _pkt_receiver_thread_func (void *arg) {
+	fd_set active_sockets, backup_sockets;
+	
+	int socket_max_fd = -1;
+	unsigned short bytes_recvd = -1;
+
+	graph_t *topo = (graph_t *)arg;
+	
+	FD_ZERO(&active_sockets); FD_ZERO(&backup_sockets);
+
+	for (auto& node : topo -> node_list ) if (node -> udp_sock_fd) {
+		if (node -> udp_sock_fd > socket_max_fd) socket_max_fd = node -> udp_sock_fd;
+
+		FD_SET(node -> udp_sock_fd, &backup_sockets);
+	}
+	
+	sockaddr_in sender_addr;
+	char *recv_buffer = (char *)malloc(sizeof(char) * MAX_PKT_BUFFER_SIZE);
+
+	while (true) {
+		memcpy(&active_sockets, &backup_sockets, sizeof(fd_set));
+		select(socket_max_fd + 1, &active_sockets, nullptr, nullptr, nullptr);
+		
+		for (auto& node : topo -> node_list)
+			if (FD_ISSET(node -> udp_sock_fd, &active_sockets)) {
+				memset(recv_buffer, 0, MAX_PKT_BUFFER_SIZE);
+				bytes_recvd = recvfrom(node -> udp_sock_fd, recvfrom, MAX_PKT_BUFFER_SIZE, 0, &sender_addr, sizeof(sockaddr));
+				_pkt_receive(node, recv_buffer, bytes_recvd);
+			}
+	}
+			
+}
+
+
+void nw_start_pkt_receiver_thread (graph_t *topo) {
+	pthread_attr_t attr;
+	pthread_t pkt_receiver_thread;
+
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+	pthread_create(&pkt_receiver_thread, &attr, _pkt_receiver_thread_func, (void *) topo);
+
+}
 
 #endif
--- src/comm.hh
+++ src/comm.hh
@@ -1,13 +1,18 @@
 #ifndef COMM_HH
 #define COMM_HH
 
+#define MAX_PKT_BUFFER_SIZE 2048
+
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <pthread.h>
 
 #include "graph.hh"
 #include "nwcli.hh"
 
 void init_udp_socket (node_t *node);
+
+void nw_start_pkt_receiver_thread (graph_t *topo);
 
 #endif
