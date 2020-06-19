4f3fd018b537a6ec14a2d56e28115a361b9767c5
Patrick Wu <magetron1@gmail.com>
2020-06-19 01:32:58 +0100

    init daemon thread

--- src/testapp.cc
+++ src/testapp.cc
@@ -5,11 +5,13 @@
 #include "nwcli.hh"
 #include "topologies.hh"
 #include "utils.hh"
+#include "comm.hh"
 
 graph_t *topo = nullptr;
 
 int main (int argc, char **argv) {
 	topo = hello_world_topo();
+	nw_start_pkt_receiver_thread(topo);
 	//dump_nw_graph(topo);
 	init_nwcli();
 	serve_nwcli();
