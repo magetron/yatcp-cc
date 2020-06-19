e38ca7639a00cb13be5667f6d26bc3640f6db6bf
Patrick Wu <magetron1@gmail.com>
2020-06-19 20:32:18 +0100

    update driver

--- src/testapp.cc
+++ src/testapp.cc
@@ -12,6 +12,17 @@ graph_t *topo = nullptr;
 int main (int argc, char **argv) {
 	topo = hello_world_topo();
 	nw_start_pkt_receiver_thread(topo);
+	
+	//wait for pkt_receiver to start
+	sleep(50);
+
+	node_t *send_node = get_node_by_node_name(topo, "R0_re");
+	interface_t *send_intf = get_node_intf_by_name(send_node, "eth0/0");
+
+	char msg[] = "Hello World!";
+	
+	send_pkt(msg, strlen(msg), send_intf);
+
 	//dump_nw_graph(topo);
 	init_nwcli();
 	serve_nwcli();
