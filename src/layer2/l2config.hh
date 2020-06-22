d301f151a35de4e15493fbd29982413432e4c78a
Patrick Wu <magetron1@gmail.com>
2020-06-22 03:18:37 +0100

    update layer2 helper

--- src/layer2/l2config.hh
+++ src/layer2/l2config.hh
@@ -5,4 +5,8 @@
 #define MAX_PAYLOAD_SIZE 1500
 #define MIN_PAYLOAD_SIZE 46
 
+#define ETH_HDR_SIZE_EXCL_PAYLOAD (sizeof(ethernet_hdr_t) - sizeof(((ethernet_hdr_t *)0)->payload))
+
+
+
 #endif
--- src/layer2/layer2.hh
+++ src/layer2/layer2.hh
@@ -10,11 +10,20 @@ struct ethernet_hdr_t {
 	//char preamble[8]; // alignment bytes with SFD, not used
 	mac_addr_t dst_addr;
 	mac_addr_t src_addr;
-	unsigned short length; // [0..1500]
-	char payload[MIN_PAYLOAD_SIZE];
+	unsigned short length; // [46..1500]
+	// TODO : Think about how to impl this dynamic payload
+	unsigned char payload[MIN_PAYLOAD_SIZE];
 	//char payload[MAX_PAYLOAD_SIZE];
-	unsigned int crc; // checksum
+	unsigned int fcs; // checksum
 };
 #pragma pack (pop)
+
+// HELPER
+#define ETH_HDR_SIZE_EXCL_PAYLOAD (sizeof(ethernet_hdr_t) - sizeof(((ethernet_hdr_t *)0)->payload))
+
+static inline ethernet_hdr_t *alloc_eth_hdr_with_payload (char *pkt, unsigned short pkt_size) {
+	return nullptr;
+}
+
 
 #endif
